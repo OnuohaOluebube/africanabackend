93ab3b70c5505d9d59e6a3b301f8a8c4d495f9d5
Oluebube <onuohaoluebube05@gmail.com>
2022-07-28 11:17:38 +0100

    Added integration test for the auth middleware

---
+++ tests/integration/middleware/auth.test.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const { Category } = require("../../../models/categories");
+const { User } = require("../../../models/user");
+
+let server;
+
+describe("auth", () => {
+  beforeEach(() => {
+    server = require("../../../index");
+  });
+
+  afterEach(async () => {
+    await server.close();
+    Category.remove({});
+  });
+
+  const exec = async () => {
+    return request(server)
+      .post("/api/categories")
+      .set("x-auth-token", token)
+      .send({ name });
+  };
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+    name = "category1";
+  });
+
+  it("should return 401 if no token is provided", async () => {
+    token = "";
+    const res = await exec();
+    expect(res.status).toBe(401);
+  });
+  it("should return 400 if token is invalid", async () => {
+    token = "1234";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+  it("should return 200 if token is valid", async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
+  });
+});
