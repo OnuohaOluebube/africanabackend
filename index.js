4851fdbb8cfe7bea9fabd1f0524ac6761a6d5bf3
Oluebube <onuohaoluebube05@gmail.com>
2022-08-21 11:21:01 +0100

    removed redundant log files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const dotenv = require("dotenv");
 dotenv.config();
-console.log(process.env);
 const express = require("express");
 const app = express();
 const logger = require("./startup/logger");
